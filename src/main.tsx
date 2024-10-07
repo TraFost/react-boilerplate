d5065ffe3146f80bc78d5aa9b9bf8b1e445f66ed
Rahman <rahwisdilfiqrak@gmail.com>
2024-10-07 13:34:52 +0700

    chore: remove strict mode on root component

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import App from "@/App.tsx";
+import "@/index.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
+	createRoot(rootElement).render(<App />);
 } else {
-  console.error("Root element not found");
+	throw new Error("Root element not found");
 }
